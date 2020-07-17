1b467383d15913f26f257b8e024654977680ae29
Tristan Emmerson <tristan@stickerslug.com>
2020-07-17 14:46:43 -0700

    add introduction to bakery message. implement pricing and promotion conveyed to user.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,19 +10,35 @@ namespace Bakery
   {
     static void Main()
     {
+      Console.WriteLine("     _|=|__________");
+      Console.WriteLine("    /               ");
+      Console.WriteLine("   /Emmerson's Bakery");
+      Console.WriteLine("  /__________________ ");
+      Console.WriteLine("   ||  || /--| ||  ||");
+      Console.WriteLine("   ||  || | .| ||  ||");
+      Console.WriteLine("   ||__||_|__|_||__||");
+      Console.WriteLine("   |-|-|-|====|-|-|-| ");
+      Console.WriteLine("^^^^^^^^^^====^^^^^^^^^^^");
       Console.WriteLine("Welcome to Emmerson's Bakery");
-      Start();
+      Console.WriteLine("----------------------------");
+      Console.WriteLine("Menu:");
+      Console.WriteLine("Loaves of Made-Fresh Bread: $5/ea");
+      Console.WriteLine("Assorted Pastries: $2/ea");
+      Console.WriteLine("____________Current Promotions_____________");
+      Console.WriteLine("Pastries Promotion: 3x for $5");
+      Console.WriteLine("Bread Promotion: Buy 2 Get 1 Free");
+      
+     /*  Start(); */
     }
 
     public static void Start()
     {
-      Console.WriteLine("welcome to bakery");
 
 
       string userResponse = Console.ReadLine();
 
 
-      Start();
+      /* Start(); */
     }
   }
 }
