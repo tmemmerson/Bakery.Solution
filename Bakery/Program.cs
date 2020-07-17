7135ebc4696b3d474631d89c167bb91149af7900
Tristan Emmerson <tristan@stickerslug.com>
2020-07-17 14:27:08 -0700

    refactor improper syntax references.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,9 +6,9 @@ using System.Linq;
 
 namespace Bakery
 {
-  class Bakery
+  public class Bakery
   {
-    public static void Main()
+    static void Main()
     {
       Console.WriteLine("Welcome to Emmerson's Bakery");
       Start();
@@ -16,27 +16,12 @@ namespace Bakery
 
     public static void Start()
     {
-      Console.WriteLine("Would you like to add an item to your list or view your list? Add/View");
+      Console.WriteLine("welcome to bakery");
+
+
       string userResponse = Console.ReadLine();
 
-/*       if (userResponse.ToUpper() == "ADD")
-      {
-        Console.WriteLine("Enter the description of your to-do item: ");
-        Item newItem = new Item(Console.ReadLine());
-      }
-      else if (userResponse.ToUpper() == "VIEW")
-      {
-      List<Item> viewStuff = (Item.GetAll());
-      
-      foreach (Item thisItem in viewStuff)
-      {
-        Console.WriteLine(thisItem.Description);
-      }
-      }
-      else
-      {
-        Console.WriteLine("Please enter either 'Add' or 'View'");
-      } */
+
       Start();
     }
   }
