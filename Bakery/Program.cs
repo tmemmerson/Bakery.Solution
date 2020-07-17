d152827c50e12d7431606d0624d9e58417ba8099
Tristan Emmerson <tristan@stickerslug.com>
2020-07-17 15:38:23 -0700

    add prompts to user for order quantity of pastries and loaves.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -21,22 +21,35 @@ namespace Bakery
       Console.WriteLine("^^^^^^^^^^====^^^^^^^^^^^");
       Console.WriteLine("Welcome to Emmerson's Bakery");
       Console.WriteLine("----------------------------");
+      Console.WriteLine(" ");
       Console.WriteLine("Menu:");
+      Console.WriteLine(" ");
       Console.WriteLine("Loaves of Made-Fresh Bread: $5/ea");
       Console.WriteLine("Assorted Pastries: $2/ea");
-      Console.WriteLine("____________Current Promotions_____________");
-      Console.WriteLine("Pastries Promotion: 3x for $5");
-      Console.WriteLine("Bread Promotion: Buy 2 Get 1 Free");
-      Console.WriteLine("___________________________________________");
+      Console.WriteLine(" ");
+      Console.WriteLine(" ____________Current Promotions_____________");
+      Console.WriteLine("|                                           |");
+      Console.WriteLine("|     Pastries Promotion: 3x for $5         |");          
+      Console.WriteLine("|     Bread Promotion: Buy 2 Get 1 Free     |");
+      Console.WriteLine("|___________________________________________|");
       Console.WriteLine("Would you like to place an order? YES/NO");
       string orderResponse =  Console.ReadLine();
       if (orderResponse == "YES" || orderResponse == "yes" || orderResponse == "Yes") //need to implement toUpper to standardize string evaluation
       {
-        Console.WriteLine("yes I want an order");
+        Console.WriteLine("How many loaves of bread would you like to purchase?");
+        string breadResponse = Console.ReadLine();
+        Console.WriteLine("Bread Loaves: " + breadResponse);
+        Console.WriteLine("How many pastries would you like to purchase?");
+        string pastryResponse = Console.ReadLine();
+        Console.WriteLine("Bread Loaves: " + breadResponse);
+        Console.WriteLine("Pastries: " + pastryResponse);
+        int breadCount = Int32.Parse(breadResponse);
+        Console.WriteLine(breadCount);
+      
       }
       else
       {
-        Console.WriteLine("no thanks");
+        Console.WriteLine("Please Come Again!");
       }
         
 
