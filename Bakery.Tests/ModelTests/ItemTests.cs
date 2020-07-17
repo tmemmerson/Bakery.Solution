0b82f26df893eff3cc95e2cf3d31dbb061894530
Tristan Emmerson <tristan@stickerslug.com>
2020-07-17 10:58:16 -0700

    change course.

--- Bakery.Tests/ModelTests/ItemTests.cs
+++ Bakery.Tests/ModelTests/ItemTests.cs
@@ -11,16 +11,8 @@ namespace Bakery.TestsTools
   public class ItemTests
   {
     [TestMethod]
-    public void OrderPrompt_PromptingTheCustomerForOrderItems_Yes()
+    public void 
     {
-/*       Console.WriteLine("Welcome to Emmerson's Bakery!");
-      Console.WriteLine("______________________________");
-      Console.WriteLine("A loaf of bread is $5, or buy 2 and get 1 free!");
-      Console.WriteLine("Our pastries are $2 or 3 for $5");
-      Console.WriteLine("______________________________"); */
-      Console.WriteLine("Would you like to place an order? Yes/No");
-      string orderAnswer = Console.ReadLine();
-      Assert.AreEqual("YES", orderAnswer);
     }
 
   }
