ec9f9a6468472e3db07a70e1d6a2f7315e5fb8ec
Tristan Emmerson <tristan@stickerslug.com>
2020-07-17 10:34:38 -0700

    add initial console greeting.

--- Bakery.Tests/ModelTests/ItemTests.cs
+++ Bakery.Tests/ModelTests/ItemTests.cs
@@ -7,9 +7,15 @@ namespace Bakery.Tests
   public class ItemTests
   {
     [TestMethod]
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void OrderPrompt_PromptingTheCustomerForOrderItems_Yes()
     {
-      // any necessary logic to prep for test; instantiating new classes, etc.
+      Console.WriteLine("Welcome to Emmerson's Bakery!")
+      Console.WriteLine("______________________________")
+      Console.WriteLine("A loaf of bread is $5, or buy 2 and get 1 free!")
+      Console.WriteLine("Our pastries are $2 or 3 for $5")
+      Console.WriteLine("______________________________")
+      Console.WriteLine("Would you like to place an order? Yes/No")
+
       Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
     }
 
---
+++ Bakery/Program.cs
@@ -0,0 +1,43 @@
+using Bakery;
+using Bakery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bakery
+{
+  class Bakery
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Emmerson's Bakery");
+      Start();
+    }
+
+    public static void Start()
+    {
+      Console.WriteLine("Would you like to add an item to your list or view your list? Add/View");
+      string userResponse = Console.ReadLine();
+
+      if (userResponse.ToUpper() == "ADD")
+      {
+        Console.WriteLine("Enter the description of your to-do item: ");
+        Item newItem = new Item(Console.ReadLine());
+      }
+      else if (userResponse.ToUpper() == "VIEW")
+      {
+      List<Item> viewStuff = (Item.GetAll());
+      
+      foreach (Item thisItem in viewStuff)
+      {
+        Console.WriteLine(thisItem.Description);
+      }
+      }
+      else
+      {
+        Console.WriteLine("Please enter either 'Add' or 'View'");
+      }
+      Start();
+    }
+  }
+}
