f58e131646b92ab30f97ae5fcd1504ef231e5f19
Tristan Emmerson <tristan@stickerslug.com>
2020-07-19 12:09:30 -0700

    refactor salutation into isolated class and added call to salutation to program.cs.

---
+++ Bakery/Models/Salutation.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bakery.Logic
+{
+  public class Salutation
+  {
+  public static void salutationPrintout ()
+    {
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("     888888 88  88    db    88b 88 88  dP     Yb  dP  dP''Yb 88   88");
+      Console.WriteLine("       88   88  88   dPYb   88Yb88 88odP       YbdP  dP   Yb 88   88");
+      Console.WriteLine("       88   888888  dP__Yb  88 Y88 88'Yb        8P   Yb   dP Y8   8P");
+      Console.WriteLine("       88   88  88 dP'  'Yb 88  Y8 88  Yb      dP     YbodP  `YbodP'");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("                            _|=|__________");
+      Console.WriteLine("                           /               ");
+      Console.WriteLine("                          /Emmerson's Bakery");
+      Console.WriteLine("                         /__________________ ");
+      Console.WriteLine("                          ||  || /--| ||  ||");
+      Console.WriteLine("                          ||  || | .| ||  ||");
+      Console.WriteLine("                          ||__||_|__|_||__||");
+      Console.WriteLine("                          |-|-|-|====|-|-|-| ");
+      Console.WriteLine("                       ^^^^^^^^^^====^^^^^^^^^^^");
+      Console.WriteLine(" ");
+      Console.WriteLine("                     ----------------------------");      
+      Console.WriteLine("                          Please Come Again!");
+      Console.WriteLine("                     ----------------------------");
+      }
+  }
+}
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -42,9 +42,6 @@ namespace Bakery
         int breadTotal = breadCount + freeBread;
         int pastryBundlesCost = CalculatePastryBundleCost(pastryCount);
 
-        
-        
-        
         int pastrySingles = pastryCount % 3;
         int pastrySinglesCost = pastrySingles * 2;
         int pastryTotalCost = pastrySinglesCost + pastryBundlesCost;
@@ -59,55 +56,24 @@ namespace Bakery
         Console.WriteLine("| Loaves Total: $" + breadTotalCost +".00");
         Console.WriteLine("|___________________________________|");
         Console.WriteLine("|                                   |");
-        Console.WriteLine("| Pastries (3 ct.): "); //pastryBundles
+        Console.WriteLine("| Pastries (3 ct.): "); Salu
         Console.WriteLine("| Pastries (1 ct.): " + pastrySingles);
         Console.WriteLine("| Total Quantity: " + pastryCount);
         Console.WriteLine("| Pastries Total: $" + pastryTotalCost +".00");
         Console.WriteLine("|___________________________________|");
         Console.WriteLine("| ORDER TOTAL PRICE: $" + receiptTotal + ".00");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("     888888 88  88    db    88b 88 88  dP     Yb  dP  dP''Yb 88   88");
-        Console.WriteLine("       88   88  88   dPYb   88Yb88 88odP       YbdP  dP   Yb 88   88");
-        Console.WriteLine("       88   888888  dP__Yb  88 Y88 88'Yb        8P   Yb   dP Y8   8P");
-        Console.WriteLine("       88   88  88 dP'  'Yb 88  Y8 88  Yb      dP     YbodP  `YbodP'");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("                            _|=|__________");
-        Console.WriteLine("                           /               ");
-        Console.WriteLine("                          /Emmerson's Bakery");
-        Console.WriteLine("                         /__________________ ");
-        Console.WriteLine("                          ||  || /--| ||  ||");
-        Console.WriteLine("                          ||  || | .| ||  ||");
-        Console.WriteLine("                          ||__||_|__|_||__||");
-        Console.WriteLine("                          |-|-|-|====|-|-|-| ");
-        Console.WriteLine("                       ^^^^^^^^^^====^^^^^^^^^^^");
-        Console.WriteLine(" ");
-        Console.WriteLine("                     ----------------------------");      
-        Console.WriteLine("                          Please Come Again!");
-        Console.WriteLine("                     ----------------------------");
+        Salutation.salutationPrintout();
       }
       else
       {
         Console.WriteLine("Please Come Again!");
       }
         
-
-
-
-     /*  Start(); */
     }
 
     public static void Start()
     {
-
-
       string userResponse = Console.ReadLine();
-
-
-      /* Start(); */
     }
   }
 }
