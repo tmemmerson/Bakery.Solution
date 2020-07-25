f5b4fa1730a3e8ea2f25f9698bf09c3e01dd7b5d
Tristan Emmerson <tristan@stickerslug.com>
2020-07-24 21:08:58 -0700

    Red flag BreadCustomerCost_CalculateCostToCustomer.

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,19 +9,31 @@ namespace Bakery.Tests
   [TestClass]
   public class BakeryTests
   {
+
     [TestMethod]
     public void FreeBread_CalculateFreeBreadPromoCount_FreeBreadCalc()
     {
     BreadItem newBreadItem = new BreadItem(14);
+    
     Assert.AreEqual(7, newBreadItem.FreeBreadCalc(14));
     }
+
 
     [TestMethod]
     public void BreadTotalCount_CalculateTotalLoavesOwed_BreadTotalCount()
     {
     BreadItem newBreadItem = new BreadItem(14);
+
     Assert.AreEqual(21, newBreadItem.BreadTotalCount(14,7));
     }
-    
+
+
+    [TestMethod]
+    public void BreadCustomerCost_CalculateCostToCustomer_BreadCustomerCost()
+    {
+    BreadItem newBreadItem = new BreadItem(14);
+
+    Assert.AreEqual(70, newBreadItem.BreadCustomerCost(14));
+    }
   }
 }
