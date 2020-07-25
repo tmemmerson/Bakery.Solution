b1b909edb70a2e24bfa976b6e3dab15968a7fe5b
Tristan Emmerson <tristan@stickerslug.com>
2020-07-24 21:22:14 -0700

    empty PastryBundleCalc method.

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -13,35 +13,35 @@ namespace Bakery.Models
       PastryCount = pastryCount;
     }
 
-    public static int PastryBundleCalc(int pastryCount)
+    public int PastryBundleCalc(int pastryCount)
     {
-      int pastryCountBundles = pastryCount / 3;
-      return pastryCountBundles;
+ /*      int pastryCountBundles = pastryCount / 3;
+      return pastryCountBundles; */
     }
 
-    public static int PastrySinglesCalc(int pastryCount)
+    public int PastrySinglesCalc(int pastryCount)
     {
       int pastryCountSingles = pastryCount % 3;
       return pastryCountSingles;
     }
 
-    public static int PastryBundleCostCalc(int pastryCount)
+    public int PastryBundleCostCalc(int pastryCount)
     {
       int pastryBundleCost = (pastryCount / 3) * 5;
       return pastryBundleCost;
     }
 
-    public static int PastrySinglesCostCalc(int pastryCount)
+    public int PastrySinglesCostCalc(int pastryCount)
     {
       int pastrySinglesCost = (pastryCount % 3) * 2;
       return pastrySinglesCost;
     }
-      public static int PastryCustomerCost(int pastryCountSingles, int pastryCountBundles)
+      public int PastryCustomerCost(int pastryCountSingles, int pastryCountBundles)
     {
       int pastryCustomerCost = (pastryCountSingles * 2) + (pastryCountBundles * 5);
       return pastryCustomerCost;
     }
-    public static int PastryTotalCount(int pastryCount)
+    public int PastryTotalCount(int pastryCount)
     {
       int pastryTotalCount = pastryCount;
       return pastryTotalCount;
