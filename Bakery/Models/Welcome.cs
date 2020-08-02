bb72e98a3836f2cc088d8a17a351113a5b7f8b38
Tristan Emmerson <tristan@stickerslug.com>
2020-08-02 13:26:04 -0700

    refactor code using static system.console for writelines optimization.

--- Bakery/Models/Welcome.cs
+++ Bakery/Models/Welcome.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Console;
 
 namespace Bakery.Logic
 {
@@ -8,33 +9,7 @@ namespace Bakery.Logic
   {
   public static void welcomePrintout ()
     {
-      Console.WriteLine("             _|=|__________");
-      Console.WriteLine("            /               ");
-      Console.WriteLine("           /Emmerson's Bakery");
-      Console.WriteLine("          /__________________ ");
-      Console.WriteLine("           ||  || /--| ||  ||");
-      Console.WriteLine("           ||  || | .| ||  ||");
-      Console.WriteLine("           ||__||_|__|_||__||");
-      Console.WriteLine("           |-|-|-|====|-|-|-| ");
-      Console.WriteLine("        ^^^^^^^^^^====^^^^^^^^^^^");
-      Console.WriteLine(" ");
-      Console.WriteLine("      ----------------------------");      
-      Console.WriteLine("      Welcome to Emmerson's Bakery");
-      Console.WriteLine("      ----------------------------");
-      Console.WriteLine(" ");
-      Console.WriteLine(" ____________________MENU____________________");
-      Console.WriteLine("|                                            |");
-      Console.WriteLine("|     Loaves of Freshly Made Bread: $5/ea    |");
-      Console.WriteLine("|     Assorted Pastries: $2/ea               |");
-      Console.WriteLine("|                                            |");
-      Console.WriteLine("|____________________________________________|");
-      Console.WriteLine(" ");
-      Console.WriteLine(" _____________Current Promotions_____________");
-      Console.WriteLine("|                                            |");
-      Console.WriteLine("|      Pastries Promotion: 3x for $5         |");          
-      Console.WriteLine("|      Bread Promotion: Buy 2 Get 1 Free     |");
-      Console.WriteLine("|____________________________________________|");
+      WriteLine("             _|=|__________\n            /               \n           /Emmerson's Bakery\n          /__________________ \n           ||  || /--| ||  ||\n           ||  || | .| ||  ||\n           ||__||_|__|_||__||\n           |-|-|-|====|-|-|-| \n        ^^^^^^^^^^====^^^^^^^^^^^\n \n      ----------------------------\n      Welcome to Emmerson's Bakery\n      ----------------------------\n \n ____________________MENU____________________\n|                                            |\n|     Loaves of Freshly Made Bread: $5/ea    |\n|     Assorted Pastries: $2/ea               |\n|                                            |\n|____________________________________________|\n \n _____________Current Promotions_____________\n|                                            |\n|      Pastries Promotion: 3x for $5         |\n|      Bread Promotion: Buy 2 Get 1 Free     |\n|____________________________________________|");
     }
   }
-
   }
