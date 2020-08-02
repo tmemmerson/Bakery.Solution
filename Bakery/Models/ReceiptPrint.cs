9c4f0d39fe75b45dccdc68fbec715ca5ab3a97d8
Tristan Emmerson <tristan@stickerslug.com>
2020-08-02 13:25:48 -0700

    refactor code using static system.console for writelines optimization.

--- Bakery/Models/ReceiptPrint.cs
+++ Bakery/Models/ReceiptPrint.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using Bakery.Models;
 using Bakery.Logic;
+using static System.Console;
 
 namespace Bakery.Logic
 {
   public class ReceiptPrint
   {
   public static void receiptPrintoutBread (int breadCount, int freeBread, int breadTotal, int breadTotalCost, int receiptTotal, int pastryCount, int pastryCustomerCost, int pastryBundleReceiptPrint, int pastryCountSingles)
-    {   
-
-        Console.WriteLine("| ORDER TOTAL PRICE: $" + receiptTotal + ".00");
-  
-        Console.WriteLine("| ORDER TOTAL PRICE: $" + receiptTotal + ".00");
+    {
+        WriteLine("| ORDER TOTAL PRICE: $" + receiptTotal + ".00");
+        WriteLine("| ORDER TOTAL PRICE: $" + receiptTotal + ".00");
     }
   }
-
   }
